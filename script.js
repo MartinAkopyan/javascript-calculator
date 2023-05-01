431fdfe996dc4e99714510eb784efd6f7181e43e
mrtn <martinakopyan34@gmail.com>
2023-05-01 13:13:57 +0300

    add operator function

--- script.js
+++ script.js
@@ -13,3 +13,27 @@ function multiply(a, b) {
 function divide(a, b) {
 	return a / b;
 }
+
+function operate(firstNum, secondNum, operator) {
+	if(operator === '+') {
+		return add(firstNum, secondNum);
+	}
+
+	if (operator === '-') {
+		return substract(firstNum, secondNum);
+	}
+
+	if(operator === '*') {
+		return multiply(firstNum, secondNum)
+	}
+
+	if (operator === '/') {
+		return divide(firstNum, secondNum)
+	}
+}
+let firstOperand = 15;
+let operator = '-';
+let secondOperand = 5;
+
+console.log(operate(firstOperand, secondOperand, operator))
+
